f6c37a90b85d9fd9d096f5ca1987904c410d67aa
BanisBT <banisbt@gmail.com>
2021-07-13 08:58:44 +0300

    first commit

---
+++ src/test/java/lt/tomasbarauskas/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.tomasbarauskas.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
